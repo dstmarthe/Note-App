53297402d4a2dfb4b502f60f46d5e280ece6795b
Dale <72896959+dstmarthe@users.noreply.github.com>
2021-05-11 01:13:49 -0400

    colon

--- pdo.php
+++ pdo.php
@@ -3,7 +3,7 @@
 $dbhost  = getenv('dbhost');
 $psw = getenv('psw');
 $user = getenv('user');
-$dbname getenv('dbname')
+$dbname getenv('dbname');
 $pdo = new PDO("mysql:host=$dbhost; dbname=$dbname",
 $user, $psw);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
