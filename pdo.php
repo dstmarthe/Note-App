951f13c981859db60a18db2900fed8d4442ba21e
Dale <72896959+dstmarthe@users.noreply.github.com>
2021-05-11 11:53:45 -0400

    pdo update

--- pdo.php
+++ pdo.php
@@ -5,7 +5,7 @@ $dbhost  = $url["host"];
 $psw = $url["pass"];
 $user = $url["user"];
 $dbname = getenv("dbname");
-$pdo = new PDO("mysql:host=",$dbhost,";dbname=",$dbname,
+$pdo = new PDO("mysql:host=$dbhost; dbname=$dbname;",
 $user, $psw);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
