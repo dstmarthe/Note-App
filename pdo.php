38d8ccef386a08f2b724c7c8d78f577b9a5f4a06
Dale <72896959+dstmarthe@users.noreply.github.com>
2021-05-11 07:02:17 -0400

    pdo connection change

--- pdo.php
+++ pdo.php
@@ -1,10 +1,11 @@
 <?php
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 // Config variables
-$dbhost  = getenv('dbhost');
-$psw = getenv('psw');
-$user = getenv('user');
-$dbname getenv('dbname');
-$pdo = new PDO("mysql:host=".$dbhost. ";dbname=".$dbname.";",
+$dbhost  = $url["host"];
+$psw = $url["pass"];
+$user = $url["user"];
+$dbname substr($url["path"], 1);
+$pdo = new PDO("mysql:host=".$dbhost.";dbname=".$dbname.";",
 $user, $psw);
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
